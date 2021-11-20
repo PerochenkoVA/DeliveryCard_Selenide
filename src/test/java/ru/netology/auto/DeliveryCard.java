d3fb533a289a0ade0e0e84320c0ca4f231ddef47
Uzbek150885 <Uzbek150885@gmail.com>
2021-11-21 01:18:15 +0300

    date

--- src/test/java/ru/netology/auto/DeliveryCard.java
+++ src/test/java/ru/netology/auto/DeliveryCard.java
@@ -3,9 +3,10 @@ package ru.netology.auto;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
-
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -16,7 +17,7 @@ public class DeliveryCard {
     void taskOne() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Петропавловск-Камчатский"); //город
-        $("[data-test-id=date] input").setValue("21.11.2021"); //дата
+        $("[data-test-id=date] input").setValue(formatDate); //дата
         $("[data-test-id=name] input").setValue("Иванов Владимир"); // ФИО
         $("[data-test-id=phone] input").setValue("+79163334455"); // telefon number
         $("[data-test-id=agreement]").click(); //согласие
@@ -25,5 +26,12 @@ public class DeliveryCard {
         $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(50));
 
     }
+
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    String date = LocalDate.now().plusWeeks(2).toString();
+    String formatDate = String.format(date, formatter);
+
+
 
 }
