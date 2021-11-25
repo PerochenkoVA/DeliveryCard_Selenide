2d85ec03aff87e0f4e1eafe2471b475be6f74325
Uzbek150885 <Uzbek150885@gmail.com>
2021-11-25 22:31:36 +0300

    doubleTest

--- src/test/java/ru/netology/auto/DeliveryCard.java
+++ src/test/java/ru/netology/auto/DeliveryCard.java
@@ -1,6 +1,7 @@
 package ru.netology.auto;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -8,30 +9,40 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryCard {
 
-
+    LocalDate date = LocalDate.now().plusDays(5);
+    LocalDate date1 = LocalDate.now();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    String date = LocalDate.now().plusWeeks(2).toString();
-    String formatDate = String.format(date, formatter);
-
 
     @Test
     void taskOne() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Петропавловск-Камчатский"); //город
-        $("[data-test-id=date] input").clear(); //очитка поля дата
-        $("[data-test-id=date] input").setValue(formatDate); //дата
+        $("[data-test-id=date] input").doubleClick(); //очитка поля дата
+        $("[data-test-id=date] input").sendKeys(" "); //дата
+        $("[data-test-id=date] input").sendKeys(formatter.format(date)); //дата
         $("[data-test-id=name] input").setValue("Иванов Владимир"); // ФИО
         $("[data-test-id=phone] input").setValue("+79163334455"); // telefon number
         $("[data-test-id=agreement]").click(); //согласие
         $("[class='button__content']").click(); //Продолжить
-
         $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(50));
 
     }
 
+    @Test
+    void taskTwo() {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Петропавловск-Камчатский"); //город
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE); //очитка поля дата
+        $("[data-test-id=date] input").setValue(formatter.format(date1)); //дата
+        $("[data-test-id=name] input").setValue("Иванов Владимир"); // ФИО
+        $("[data-test-id=phone] input").setValue("+79163334455"); // telefon number
+        $("[data-test-id=agreement]").click(); //согласие
+        $("[class='button__content']").click(); //Продолжить
+        $(withText("Заказ на выбранную дату невозможен")).shouldBe(visible);
+
+    }
 }
