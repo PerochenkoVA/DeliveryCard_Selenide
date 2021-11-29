843d1b979a0d80e5fd39d6d899c134fde9ab57b3
Uzbek150885 <Uzbek150885@gmail.com>
2021-11-29 20:09:28 +0300

    triplTest

--- src/test/java/ru/netology/auto/DeliveryCard.java
+++ src/test/java/ru/netology/auto/DeliveryCard.java
@@ -7,6 +7,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
@@ -45,4 +46,25 @@ public class DeliveryCard {
         $(withText("Заказ на выбранную дату невозможен")).shouldBe(visible);
 
     }
+
+    String generateDate(int days, String datePattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(datePattern));
+    }
+
+    @Test
+    void shouldSendValidForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Петропавловск-Камчатский");
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").setValue(planningDate);
+        $("[data-test-id=name] input").setValue("Иванов Владимир");
+        $("[data-test-id=phone] input").setValue("+79163334455");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+    }
+
 }
